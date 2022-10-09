a4251e8f49f30610a4fe056f2738659d4dd9938d
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2022-10-09 10:50:03 +0100

    New commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var opacity = 0;
 var fadeInterval = setInterval(fadeIn, 1);
 
+
 function fadeIn() {
     if (opacity < 0.5) {
         opacity += 0.005;
@@ -13,3 +14,8 @@ function fadeIn() {
     }
     document.body.style.opacity = opacity;
 }
+
+particlesJS.load('particles-js', 'particles.json')
+
+const box = document.getElementsByClassName("box");
+console.log(box.style.opacity)
