6bc0c8f34cd8ee13dc5c7b2919ee1df90a7a213a
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-02-23 14:35:01 +0000

    Removed fade in

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-var opacity = 0;
-var fadeInterval = setInterval(fadeIn, 1);
-const box = document.getElementsByClassName("box")[0];
+// const box = document.getElementsByClassName("box")[0];
+// let opacity = 0;
+// let fadeInterval = setInterval(fadeIn, 1);
 
-function fadeIn() {
-    if (opacity < 0.5) {
-        opacity += 0.005;
-    }
-    else if (opacity < 1) {
-        opacity += 0.0025;
-    }
-    else {
-        clearInterval(fadeInterval)
-    }
-    box.style.opacity = opacity;
-}
+// function fadeIn() {
+//     if (opacity < 0.5) {
+//         opacity += 0.005;
+//     } else if (opacity < 1) {
+//         opacity += 0.0025;
+//     } else {
+//         clearInterval(fadeInterval);
+//     }
+//     box.style.opacity = opacity;
+// }
 
-particlesJS.load('particles-js', 'particles.json')
+particlesJS.load('particles-js', 'particles.json');
